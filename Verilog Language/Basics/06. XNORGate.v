f76bb2253dc1cc7475db98d67e8f0dadd16baccc
Varshinin87 <96334739+Varshinin87@users.noreply.github.com>
2025-03-22 03:50:57 -0400

    Update and rename XNORGate.v to 06. XNORGate.v

--- Verilog Language/Basics/06. XNORGate.v
+++ Verilog Language/Basics/06. XNORGate.v
@@ -1,6 +1,3 @@
-// Description: This module implements an XNOR gate.
-//              The output is the logical XNOR of the two input signals.
-
 module top_module( 
     input a,  // First input
     input b,  // Second input
