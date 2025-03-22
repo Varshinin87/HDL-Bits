ae7b2f132a58f0a749189930f2681f7e4bdee2ff
Varshinin87 <96334739+Varshinin87@users.noreply.github.com>
2025-03-22 03:54:40 -0400

    Update and rename vector2.v to 11. VectorPartSelect.v

--- Verilog Language/Vectors/11. VectorPartSelect.v
+++ Verilog Language/Vectors/11. VectorPartSelect.v
@@ -1,7 +1,3 @@
-// Description: This module performs a byte-wise swap on a 32-bit input signal 'in' and outputs the result as 'out'.
-//              The output bytes are swapped such that:
-//              out[7:0] = in[31:24], out[15:8] = in[23:16], out[23:16] = in[15:8], and out[31:24] = in[7:0].
-
 module top_module( 
     input [31:0] in,   // 32-bit input vector
     output [31:0] out  // 32-bit output vector after byte swapping
