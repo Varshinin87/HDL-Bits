e68a1321fa1cfd04d3b8eeafa0ea072dceceaca5
Varshinin87 <96334739+Varshinin87@users.noreply.github.com>
2025-03-22 03:48:29 -0400

    Update and rename SimpleWire.v to 01. SimpleWire.v

--- Verilog Language/Basics/01. SimpleWire.v
+++ Verilog Language/Basics/01. SimpleWire.v
@@ -1,5 +1,3 @@
-// Description: This is a simple pass-through module where the input signal is directly assigned to the output.
-
 module top_module( input in, output out );  
 
     // Assign the input 'in' directly to the output 'out'
